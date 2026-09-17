e30cb892d7327015a3364b6950be783a1ba1e457
resham-dilawari <resham-dilawari@users.noreply.github.com>
2026-09-17 23:29:43 +0530

    Update info message text since sidebar is removed

--- app_multiagent.py
+++ app_multiagent.py
@@ -385,7 +385,7 @@ def main():
             </div>
             """, unsafe_allow_html=True)
         
-        st.info("👈 Configure your portfolio in the sidebar and click 'Run Multi-Agent Analysis' to begin!")
+        st.info("Configure your portfolio above and click 'Run Multi-Agent Analysis' to begin!")
         
         # Show execution history if available
         if st.session_state.execution_history:
